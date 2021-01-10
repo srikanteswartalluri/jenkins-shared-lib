6a2396019b6f65f9bfaf0d843cd3afc07014eb6c
Srikanteswararao Talluri <srikanteswararaotalluri@apples-MacBook-Pro.local>
2021-01-10 23:02:05 +0530

    add default parameter

---
+++ vars/commonPipeline.groovy
@@ -0,0 +1,12 @@
+def call(int buildNumber=10) {
+    pipeline {
+      agent any
+      stages {
+        stage('Odd Stage') {
+          steps {
+            echo "The build number is odd"
+          }
+        }
+      }
+    }
+}
